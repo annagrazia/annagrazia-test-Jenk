7be042b3f3b504543b6cf322c1c05aa8106a921a
Anna Grazia <anna.occhineri@tech.agilex.it>
2021-05-15 12:29:33 +0200

    First hello app with Jenkins

---
+++ test.py
@@ -0,0 +1,27 @@
+import unittest
+import hello
+
+class TestHello(unittest.TestCase):
+
+    def setUp(self):
+        hello.app.testing = True
+        self.hello = hello.app.test_client()
+
+    def test_hello(self):
+        rv = self.hello.get('/')
+        self.assertEqual(rv.status, '200 OK')
+        self.assertEqual(rv.data, b'Hello World!\n')
+
+    def test_hello_hello(self):
+        rv = self.hello.get('/hello/')
+        self.assertEqual(rv.status, '200 OK')
+        self.assertEqual(rv.data, b'Hello World!\n')
+
+    def test_hello_name(self):
+        name = 'Simon'
+        rv = self.hello.get(f'/hello/{name}')
+        self.assertEqual(rv.status, '200 OK')
+        self.assertIn(bytearray(f"{name}", 'utf-8'), rv.data)
+
+if __name__ == '__main__':
+    unittest.main()
